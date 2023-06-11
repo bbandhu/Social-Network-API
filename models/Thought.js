a520af6e2aea4beaec44cb28c44aed4a80d8cc48
bbandhu <bandhavi.b12@gmail.com>
2023-06-11 06:44:00 -0700

    models

--- models/Thought.js
+++ models/Thought.js
@@ -1,13 +1,19 @@
-const mongoose = require('mongoose');
-const ReactionSchema = require('./reaction');
+const mongoose = require("mongoose");
+const ReactionSchema = require("./Reaction");
 
 const ThoughtSchema = new mongoose.Schema({
-    thoughtText: String,
-    createdAt: { type: Date, default: Date.now },
-    username: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    reactions: [ReactionSchema]
+  thoughtText: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  username: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  reactions: [ReactionSchema],
 });
 
-const Thought = mongoose.model('Thought', ThoughtSchema);
+const Thought = mongoose.model("Thought", ThoughtSchema);
 
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -4,8 +4,14 @@ const {mongoose, Types} = require('mongoose');
 const UserSchema = new mongoose.Schema({
     username: String,
     email: String,
-    thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Thought' }],
-    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+    thoughts: [{
+         type: mongoose.Schema.Types.ObjectId, 
+         ref: 'Thought'
+         }],
+    friends: [{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User'
+     }]
 });
 
 const User = mongoose.model('User', UserSchema);
